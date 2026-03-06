5fb580fe5b9e735d614be1060beed45cb39a9f5a
sujitha325 <yedidasujitha43@gmail.com>
2026-03-06 14:29:00 +0000

    text added to requirements

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask,render_template,request
 import pickle
 import numpy as np
-model=pickle.load(open("model.pkl","rb"))
 app=Flask(__name__)
+model=pickle.load(open("model.pkl","rb"))
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -11,8 +11,10 @@ def predict():
     itching=int(request.form["itching"])
     skin_rash=int(request.form["skin_rash"])
     nodal_skin_eruptions=int(request.form["nodal_skin_eruptions"])
-    sneezing=int(request.form["sneezing"])
-    features=np.array([[itching,skin_rash,nodal_skin_eruptions]])
+    continuous_sneezing=int(request.form["continuous_sneezing"])
+    features=np.array([[itching,skin_rash,nodal_skin_eruptions,continuous_sneezing]])
+    #prediction
     prediction=model.predict(features)
+    return render_template("index.html",result=prediction[0])
 if __name__=="__main__":
     app.run(debug=True)
